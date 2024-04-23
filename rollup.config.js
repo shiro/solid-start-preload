32139208901338295180d9ecbb78d4f4de80b08b
shiro <shiro@usagi.io>
2024-04-23 16:14:34 +0900

    fix(release): fix exports

--- rollup.config.js
+++ rollup.config.js
@@ -3,17 +3,17 @@ import withSolid from "rollup-preset-solid";
 export default withSolid([
   {
     input: "src/index.ts",
-    targets: ["esm"],
+    targets: ["cjs", "esm"],
     solidOptions: { generate: "ssr" },
   },
   {
     input: "src/server.ts",
-    targets: ["esm"],
+    targets: ["cjs", "esm"],
     solidOptions: { generate: "ssr" },
   },
   {
     input: "src/babel.ts",
-    targets: ["esm"],
+    targets: ["cjs", "esm"],
     solidOptions: { generate: "ssr" },
   },
 ]);
